31101e1c2429e017aaff1ed3a231671c03c4f4bd
vvdspelt <vvdspelt@gmail.com>
2017-01-18 13:54:39 +0100

    Piramide gefixed
    Hij doet het weer

--- Piramide.py
+++ Piramide.py
@@ -1,7 +1,7 @@
 i = int(input("Insert a number for height: "))
 s = ''
-for y in range(0, i):
-    for x in range(0, i):
+width = 2 * i
+for y in range(1, i + 1):
     for x in range(0, width):
         if x < i + y and x > i - y:
             s += '*'
