59953b0ccfbe3fb1ec5b7dde564429418ebfb128
0926722 <0926722@hr.nl>
2017-01-19 10:57:23 +0100

    hardcoding removed

--- Boot+grid.py
+++ Boot+grid.py
@@ -5,11 +5,12 @@ clock = pygame.time.Clock()
 FPS = 60
 
 class Boat:
-    def __init__(self, x, y, length):
+    def __init__(self, x, y, length, gamegrid):
         self.x = x
         self.y = y
         self.length = length
-        self.position = (self.x, self.y, 22, (30*self.length - 8))
+        self.gamegrid = gamegrid
+        self.position = (self.x, self.y, (self.gamegrid.gridx)-8, ((self.gamegrid.gridy)*self.length - 8))
 
     def draw(self, screen):
         pygame.draw.ellipse(screen, (0, 0, 0), self.position, 0)
@@ -18,35 +19,14 @@ class Boat:
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
-            self.x += 30
+            self.x += (self.gamegrid.x/20)
         elif keys[pygame.K_LEFT]:
-            self.x -= 30
+            self.x -= (self.gamegrid.x/20)
 
         if keys[pygame.K_UP]:
-            self.y -= 30
+            self.y -= (self.gamegrid.y/20)
         elif keys[pygame.K_DOWN]:
-            self.y += 30
-
-class Grid:
-    def __init__(self, resolution_x, resolution_y):
-        self.x = 600
-        self.y = 600
-        self.resolution_x = resolution_x
-        self.resolution_y = resolution_y
-
-    def fill(self, screen):
-        pygame.draw.rect(screen, (0, 0, 180), (((self.resolution_x-self.x)/2), ((self.resolution_y-self.y)/2), self.x, self.y), 0)
-
-    def draw(self, screen):
-        gridlines = 0
-        while not gridlines > 20:
-            pygame.draw.line(screen, (0, 0, 0), (((self.resolution_x-self.x)/2), ((self.resolution_y-self.y)/2)+30*gridlines), (((self.resolution_x-self.x)/2)+self.x, ((self.resolution_y-self.y)/2)+30*gridlines), 2)
-            gridlines += 1
-        gridlines = 0
-        while not gridlines > 20:
-            pygame.draw.line(screen, (0, 0, 0), (((self.resolution_x-self.x)/2)+30*gridlines, ((self.resolution_y-self.y)/2)), (((self.resolution_x-self.x)/2)+30*gridlines, ((self.resolution_y-self.y)/2)+self.y), 2)
-            gridlines += 1
-
+            self.y += (self.gamegrid.y/20)
 
 def process_events():
     for event in pygame.event.get():
@@ -63,10 +43,7 @@ def program():
 
     GameGrid = Grid(width, height)
 
-    Boat2 = Boat(205, 205, 2)
-    Boat3 = Boat(235, 205, 3)
-    Boat4 = Boat(265, 205, 4)
-
+    Boat2 = Boat((GameGrid.gridstartx+((1/6)*GameGrid.gridx)), (GameGrid.gridstarty+((1/6)*GameGrid.gridy)), 2, GameGrid)
 
     pygame.init()
 
@@ -77,10 +54,8 @@ def program():
          screen.fill((255, 255, 255))
          GameGrid.fill(screen)
          GameGrid.draw(screen)
-         Boat2.move()
          Boat2.draw(screen)
-         Boat3.draw(screen)
-         Boat4.draw(screen)
+
          pygame.display.flip()
          clock.tick(FPS)
 
