0d12c59a27ad78085517eeedccf1914501e573fe
0926722 <0926722@hr.nl>
2017-01-19 22:29:59 +0100

    print(event) removed

--- gameIntro+gameLoop+gameRules.py
+++ gameIntro+gameLoop+gameRules.py
@@ -119,7 +119,6 @@ def button(text, x, y, width, height, inactive_color, active_color, text_color, 
     if x+width > cursor[0] > x and y+height > cursor[1] > y:
         pygame.draw.rect(screen, active_color, (x, y, width, height))
         for event in pygame.event.get():
-            print(event)
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 do_action(action)
         #if click[0] == 1 and action != None:
