c42802a9411855a4caae1bc99db1bc12a05cff86
0926722 <0926722@hr.nl>
2017-01-20 19:03:12 +0100

    fixed boat.draw_new

--- Intro+Loop+Rules+Termination+BoatPositionMethod.py
+++ Intro+Loop+Rules+Termination+BoatPositionMethod.py
@@ -99,7 +99,7 @@ class Boat:
         elif self.original_stance == "defending":
             pygame.draw.ellipse(screen, black, self.position_defending, 0)
 
-    def draw_new_stance(self, screen):
+    def draw_new(self, screen):
         if not self.original_stance == self.new_stance and self.new_stance == "defending":
             pygame.draw.ellipse(screen, light_red, (self.new_x, self.new_y, self.position_attacking[3], self.position_attacking[2]), 0)
         elif not self.original_stance == self.new_stance and self.new_stance == "attacking":
@@ -108,20 +108,22 @@ class Boat:
             else:
                 pygame.draw.ellipse(screen, light_red, (self.x, self.y, (self.gamegrid.gridx) - (self.gamegrid.gridx / 4), ((self.gamegrid.gridy) * self.length - (self.gamegrid.gridx / 4))), 0)
         elif self.original_stance == "defending" and not self.movement == self.steps:
-            pygame.draw.ellipse(screen, light_red, (self.new_x, self.new_y, self.position_attacking[3], self.position_attacking[2]), 0)
+            if self.new_stance == "defending" and self.movement == self.steps - 1:
+                pygame.draw.ellipse(screen, black, self.position_defending, 0)
+            else:
+                pygame.draw.ellipse(screen, light_red, (self.new_x, self.new_y, self.position_attacking[3], self.position_attacking[2]), 0)
         elif self.original_stance == "attacking" and not self.movement == self.steps:
             pygame.draw.ellipse(screen, light_red, (self.new_x, self.new_y, (self.gamegrid.gridx) - (self.gamegrid.gridx / 4), ((self.gamegrid.gridy) * self.length - (self.gamegrid.gridx / 4))), 0)
 
 
     def change_stance(self):
+        if self.original_stance == "defending" and self.movement == self.steps:
+            self.movement -= 1
         if self.movement > 0:
             if self.new_stance == "defending":
                 self.new_stance = "attacking"
             else:
                 self.new_stance = "defending"
-        print(self.movement)
-
-
 
     def move(self, direction):
         if self.new_stance == "attacking":
@@ -358,7 +360,7 @@ def gameLoop():
          screen.fill(white)
          GameGrid.draw(screen)
          Boat2.draw(screen)
-         Boat2.draw_new_stance(screen)
+         Boat2.draw_new(screen)
 
          button("Game beëindigen", (display_width/2)-150, (display_height*0.1), 300, 50, red, light_blue, black, "termination_screen")
          button("Hoofdmenu", display_width * 0.825, display_height * 0.85, 190, 60, green, light_blue, black, "main")
