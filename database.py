e03b806b4eff05aa765663c9557162b008dc3880
0926722 <0926722@hr.nl>
2017-01-31 10:32:11 +0100

    ?

--- database.py
+++ database.py
@@ -1,35 +1,52 @@
 import psycopg2
 
+
+# Use the database
 def interact_with_database(command):
-    connection = pyscopg2.connect("dbname=Project 2 user=postgres")
+    # Connect and set up cursor
+    connection = psycopg2.connect("dbname=Project 2 highscores user=postgres")
     cursor = connection.cursor()
 
+    # Execute the command
     cursor.execute(command)
     connection.commit()
 
+    # Save results
     results = None
     try:
         results = cursor.fetchall()
     except psycopg2.ProgrammingError:
+        # Nothing to fetch
         pass
 
+    # Close connection
     cursor.close()
     connection.close()
 
     return results
 
+
+# Uploads a score into the highscore table
 def upload_score(name, score):
-    interact_with_database("UPDATE highscore SET score = {} WHERE naam = '{}'".format(score, name))
+    interact_with_database("UPDATE Highscore SET Score = {} WHERE Naam = '{}'".format(score, name))
 
-def update_score(name, score):
-    interact_with_database("UPDATE highscore SET score = {} WHERE name = '{}'".format)(score, name)
 
-def download_highscores():
-    return interact_with_database("SELECT * FROM highscore ORDER BY score")
+# Updates existing scores
+def update_score(name):
+    interact_with_database("UPDATE Highscore SET Score = Score + 1 WHERE Naam = '{}'".format(name))
 
+
+# Downloads data from database
+def download_highscore():
+    return interact_with_database("SELECT * FROM Highscore")
+
+
+# Downloads the top score from database
+def download_top_score():
+    result = interact_with_database("SELECT * FROM Highscore ORDER BY Score")
+    return result
+
+# Check if player already has a score
 def check_name(name):
-    score = interact_with_database("SELECT score FROM highscore WHERE naam = {}".format(name))
+    score = interact_with_database("SELECT score FROM highscore where naam = '{}'".format(name))
     return score
-
-download_highscores()
-
